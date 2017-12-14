6452706b91dd345ee57ee4f15e421666b76f282e
mark <markphi119@gmail.com>
2017-12-14 12:55:32 +0300

    pay using aPI class

--- src/Controller/PaymentsController.php
+++ src/Controller/PaymentsController.php
@@ -1,8 +1,10 @@
 <?php
 namespace App\Controller;
 
+
 use App\Controller\AppController;
 use Cake\Event\Event;
+use AfricasTalkingGateway;
 
 /**
  * Payments Controller
@@ -11,7 +13,7 @@ use Cake\Event\Event;
  */
 class PaymentsController extends AppController
 {
-
+    
     /**
      * Index method
      *
@@ -113,7 +115,7 @@ class PaymentsController extends AppController
         $payment = $this->Payments->newEntity();        
         if ($this->request->is('post')) {
             $payment = $this->Payments->patchEntity($payment, $this->request->data);
-            $url = "https://payments.sandbox.africastalking.com/mobile/b2c/request";
+            //$url = "https://payments.sandbox.africastalking.com/mobile/b2c/request";
             $jdata = array(
                 'username'=> 'sandbox',
                 'productName'=> 'payroll',
@@ -125,35 +127,16 @@ class PaymentsController extends AppController
                         "amount"=>$this->request->data['amount'],
                         "reason"=> "SalaryPayment",
                         "metadata" => array(
-                        "description" => "May Salary",
-                        "employeeId" => "123"
-                        )
-                    )    
-                )
-            );
-
-            $post_data = json_encode($jdata);
-
-            $ch = curl_init();
-            // *** Not secure, not a good idea for live environment *** //
-            curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, 0); 
-            curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, 0);
-            // ******************************************************* //
-            curl_setopt($ch, CURLOPT_URL, $url);
-            curl_setopt($ch, CURLOPT_POST, true);
-            curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-            //curl_setopt($ch, CURLOPT_USERPWD, "username:password" );
-            curl_setopt($ch, CURLOPT_HTTPHEADER, array('apikey: 38a5d3364a2970ca7dc474ae85e551c6baf86c24b9d17e08b3944b946dc9101e',
-             'Content-Type: application/json'));
-            curl_setopt($ch, CURLOPT_POSTFIELDS, $post_data);            
-
-            $result = curl_exec($ch);
-            // Check for errors
-            if($result === FALSE){
-                var_dump($result);
-                die(curl_error($ch));
-            }
-            curl_close($ch);
+                            "description" => "May Salary",
+                            "employeeId" => "123"
+                            )
+                        )    
+                    )
+                );
+            
+            
+            $result = $this->doAtPost(json_encode($jdata));
+            
             $Fmessage = json_decode($result, true);
             if ($Fmessage['entries'][0]['status'] == 'Queued'){
                 $this->Payments->save($payment);                
@@ -161,6 +144,134 @@ class PaymentsController extends AppController
                 return $this->redirect(['action' => 'index']);
             }else {
                 $this->Flash->error(__('The CASH not sent. Reason: '. $Fmessage['entries'][0]['errorMessage']));
+            }
+        }
+        $this->set(compact('payment'));
+        $this->set('_serialize', ['payment']);
+    }
+    
+    private function doAtPost($post_data)
+    {
+        $url = "https://payments.sandbox.africastalking.com/mobile/b2c/request";
+        
+        $ch = curl_init();
+        // *** Not secure, not a good idea for live environment *** //
+        curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, 0); 
+        curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, 0);
+        // ******************************************************* //
+        curl_setopt($ch, CURLOPT_URL, $url);
+        curl_setopt($ch, CURLOPT_POST, true);
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+        //curl_setopt($ch, CURLOPT_USERPWD, "username:password" );
+        curl_setopt($ch, CURLOPT_HTTPHEADER, array('apikey: 38a5d3364a2970ca7dc474ae85e551c6baf86c24b9d17e08b3944b946dc9101e',
+            'Content-Type: application/json'));
+        curl_setopt($ch, CURLOPT_POSTFIELDS, $post_data); 
+        
+        $result = curl_exec($ch);
+        
+        // Check for errors
+        if($result === FALSE){
+            var_dump($result);
+            die(curl_error($ch));
+        }
+        curl_close($ch);
+        
+        return $result;
+        
+    }
+
+    public function send2()
+    {
+        require_once "AfricasTalkingGateway.php";
+
+        $payment = $this->Payments->newEntity(); 
+
+        if ($this->request->is('post')) {
+            $payment = $this->Payments->patchEntity($payment, $this->request->data);
+            //Specify your credentials
+            $username = "sandbox";
+            $apiKey   = "38a5d3364a2970ca7dc474ae85e551c6baf86c24b9d17e08b3944b946dc9101e";
+            
+            //Create an instance of our awesome gateway class and pass your credentials
+            //$gateway = new AfricasTalkingGateway($username, $apiKey);
+
+            $gateway = new AfricasTalkingGateway($username, $apiKey, "sandbox");
+            
+            /*************************************************************************************
+            NOTE: If connecting to the sandbox:
+            
+            1. Use "sandbox" as the username
+            2. Use the apiKey generated from your sandbox application
+            https://account.africastalking.com/apps/sandbox/settings/key
+            3. Add the "sandbox" flag to the constructor
+            
+            $gateway  = new AfricasTalkingGateway($username, $apiKey, "sandbox");
+            **************************************************************************************/
+            
+            // Specify the name of your Africa's Talking payment product
+            $productName  = "payroll";
+            
+            // The 3-Letter ISO currency code for the checkout amount
+            $currencyCode = "UGX";
+            
+
+            $recipient1   = array("phoneNumber" => $this->request->data['phone'],
+                "currencyCode" => $currencyCode,
+                "amount"       => $this->request->data['amount'],
+                "metadata"     => array("name"   => $this->request->data['recipient'],
+                    "reason" => "May Salary")
+                );
+            /*// Provide the details of a mobile money recipient
+            $recipient1   = array("phoneNumber" => "+254711345565",
+                "currencyCode" => "UGX",
+                "amount"       => 1050,
+                "metadata"     => array("name"   => "Clerk",
+                    "reason" => "May Salary")
+                );
+            // You can provide up to 10 recipients at a time
+            $recipient2   = array("phoneNumber"  => "+254711890754",
+                "currencyCode" => "UGX",
+                "amount"       => 5010,
+                "metadata"     => array("name"   => "Accountant",
+                    "reason" => "May Salary")
+                );*/
+
+            // Put the recipients into an array
+            //$recipients  = array($recipient1, $recipient2);
+            $recipients  = array($recipient1);
+            
+            try {
+                $responses = $gateway->mobilePaymentB2CRequest($productName, $recipients);
+                
+                foreach($responses as $response) {
+                    //$Fmessage = json_decode($result, true);
+                    if ($response->status == 'Queued'){
+                        $this->Payments->save($payment);                
+                        $this->Flash->success(__('The CASH has been sent. TransactionID: '.$response->transactionId. '   THANK U :-)'));
+                        return $this->redirect(['action' => 'index']);
+                    }else {
+                        $this->Flash->error(__('The CASH not sent. Reason: '. $response->errorMessage. ':-('));
+                    }
+                    
+                    // Parse the responses and print them out
+                    /*echo "phoneNumber=".$response->phoneNumber;
+                    echo ";status=".$response->status;
+                    
+                    if ($response->status == "Queued") {
+                        echo ";transactionId=".$response->transactionId;
+                        echo ";provider=".$response->provider;
+                        echo ";providerChannel=".$response->providerChannel;
+                        echo ";value=".$response->value;
+                        echo ";transactionFee=".$response->transactionFee."\n";
+                    } else {
+                        echo ";errorMessage=".$response->errorMessage."\n";
+                    }*/
+                }
+                
+            }
+            catch(AfricasTalkingGatewayException $e){
+                $this->Flash->error(__('The CASH not sent. Reason: '. $e->getMessage()));
+                //echo "Received error response: ".$e->getMessage();
             }
         }
         $this->set(compact('payment'));
